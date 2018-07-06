eb5038087d2e28a2f42eae88e757ec3149061864
Veda Murthy <vvmurthy@usc.edu>
2018-07-06 12:08:51 -0700

    fix variable names

--- src/main/java/assignment1/PrimeNumbers.java
+++ src/main/java/assignment1/PrimeNumbers.java
@@ -2,10 +2,16 @@ package assignment1;
 
 public class PrimeNumbers {
 
+    /**
+    NOTE: Do not change this function
+    */
     public static boolean BadPrimeExample(int n){
         return false;
     }
 
+    /**
+    NOTE: Do not change this function
+    */
     public static boolean BadPrimeNumbers1(int n){
         if(n % 2 == 1){
             return true;
@@ -14,6 +20,9 @@ public class PrimeNumbers {
         }
     }
 
+    /**
+    NOTE: Do not change this function
+    */
     public static boolean BadPrimeNumbers2(int n){
         for(int i = 1 ; i <= n ; i++){
             if(n % i == 0){
@@ -32,8 +41,8 @@ public class PrimeNumbers {
      *
      * if assignment3 == 2, return true
      *
-     * Otherwise, for all numbers in between 2 and assignment3 - 1 inclusive,
-     * if assignment3 is divisible by that number, return false.
+     * Otherwise, for all numbers in between 2 and n - 1 inclusive,
+     * if n is divisible by that number, return false.
      *
      * @param n input to determine whether it is prime
      * @return true if prime, false if not
