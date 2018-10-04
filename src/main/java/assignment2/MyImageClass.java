34f4c4488139c14c29f91f7935968ec58fb467e8
Veda Murthy <vvmurthy@usc.edu>
2018-10-04 14:38:56 -0700

    add new class-based assignment to repo

---
+++ src/main/java/assignment2/MyImageClass.java
@@ -0,0 +1,54 @@
+package assignment2;
+
+/**
+ * In this assignment, you are going to be
+ * creating a class to model an image,
+ *  without having any actual image data
+ *  in it.
+ *
+ *
+ *  TASK 1) Class data fields
+ *  Think about the differences and similarities
+ *  in an image
+ *  + How are we going to model the dimensions of an image?
+ *  Its height is some amount, its row is some amount. How
+ *  will we store that as a class variable?
+ *
+ *
+ *  TASK 2) Constructor
+ *  Write a constructor for your MyImageClass. You should
+ *  have the constructor accept enough data to fill every single
+ *  class data field from class 1.
+ *
+ *  TASK 3) Getters and setters
+ *  + create functions to return specific class fields, and
+ *  functions to set their values after construction of an object
+ *
+ *  2 Things I want you to consider:
+ *  1) Can we have an image of 0 pixel height? how about negative height?
+ *  negative width?
+ *  2) What sort of values can we store in our image? Remember a few
+ *  months ago our discussion about the unsigned integer 8 type and how
+ *  images are usually represented using the values between [0, 255].
+ *  How will your program make sure "set" image data is valid?
+ *
+ *  I am not going to check actual functionality of this program,
+ *  but rather just look at your code to see if it is designed properly.
+ */
+public class MyImageClass {
+
+    // DEFINE YOUR class fields here
+    // E.g. private int myInteger;
+
+    // DEFINE YOUR constructors here
+    // Remember the syntax of a constructor from 11.3
+
+    // Define your getters and setters here
+    // They will be the the format (for getters)
+    // public <Datatype> getMyValue(){return myValue;}
+    // where Datatype is the datatype of the class field myValue
+    // (and for setters)
+    // public void setMyValue(Datatype newMyValue){this.myValue = newMyValue;}
+
+
+}
